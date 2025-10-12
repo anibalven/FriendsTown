3b71cae8b54a65587bf8d1c6110903aea7710328
Anibal Marcano <anibalven@hotmail.com>
2025-10-12 23:29:43 +0200

    Lesson5

---
+++ FriendsTown.Web/Controllers/TestController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsTown.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public ContentResult Index()
+        {
+            string message = "Welcome to FriendsTown " +
+               $"today is {DateTime.Today}";
+            return new ContentResult
+            {
+                Content = message
+            };
+        }
+
+        public IActionResult Html()
+        {
+            return new ContentResult
+            {
+                Content = "<h1>Welcome to FriendsTown</h1>",
+                ContentType = "html"
+            };
+        }
+
+        public ContentResult Forbidden()
+        {
+            return new ContentResult
+            {
+                StatusCode = 403
+            };
+        }
+
+        public JsonResult JsonData()
+        {
+            var user = new { Id = 1, Name = "Mary" };
+            return new JsonResult(user);
+
+        }
+
+        public ContentResult ShowCode(string id)
+        {
+            return new ContentResult
+            {
+                Content = $"Received Code: {id}"
+            };
+        }
+
+        public ContentResult ProcessName(string name)
+        {
+            return new ContentResult
+            {
+                Content = $"Hi: {name} your name has " +
+                    $"{name.Length} letters "
+            };
+        }
+
+        public ContentResult NetPrice(decimal price, decimal discount)
+        {
+            decimal calculatedDiscount = price * discount / 100;
+            decimal netPrice = price - calculatedDiscount;
+            return new ContentResult
+            {
+                Content = $"Price: {price} discount: {calculatedDiscount} " +
+                    $"Net Price: {netPrice}"
+            };
+        }
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# FriendsTown
-Sample application for the book PROGRAMMING  ASP.NET CORE 9  
-LESSON 1:  https://github.com/anibalven/FriendsTown/tree/Lesson1  
-LESSON 2:  https://github.com/anibalven/FriendsTown/tree/Lesson2  
-
