86c1845908c0022c6beaaf18a639355a406c480a
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:51:29 +0200

    Lesson16

---
+++ FriendsTown.Domain/Date.cs
@@ -0,0 +1,43 @@
+﻿namespace FriendsTown.Domain;
+
+public class Date : ValueObject
+{
+    private Date() { }
+
+    private Date(DateTime date)
+    {
+        if (date.CompareTo(DateTime.Now) <= 0)
+        {
+            throw new ArgumentException
+                ("Date must be greater than today");
+        }
+        Value = date;
+    }
+
+    public DateTime Value { get; init; }
+
+    public static Date FromString(string stringDate)
+    {
+        if (DateTime.TryParse(stringDate, out var converted))
+        {
+            return new Date(converted);
+        }
+        else
+        {
+            throw new ArgumentException("Incorrect date");
+        }
+    }
+
+    public static Date FromDate(DateTime date) => new Date(date);
+
+    public int RemainingDays => (Value - DateTime.Now).Days;
+
+    public string Announcement => $"We wait for you on " +
+        $" {Value.ToShortDateString()}";
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+
---
+++ FriendsTown.Web/Controllers/ActivityController.cs
@@ -0,0 +1,103 @@
+﻿using FriendsTown.Data.Repositories;
+using FriendsTown.Domain;
+using FriendsTown.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsTown.Web.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private readonly IActivityRepository _activityRepository;
+        public ActivityController(IActivityRepository
+                                    activityRepository)
+        {
+            _activityRepository = activityRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var activities = _activityRepository.GetAll();
+            var model = activities.Select(a => new ActivityViewModel
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Description = a.Description
+            });
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var activity = _activityRepository.FindById(id);
+            var model = new ActivityViewModel
+            {
+                Id = id,
+                Name = activity.Name,
+                Description = activity.Description
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ActivityViewModel model)
+        {
+            var actividad = Activity.Create(model.Id,
+                model.Name, model.Description);
+            _activityRepository.Update(actividad);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Details(Guid id)
+        {
+            var activity = _activityRepository.FindById(id);
+            var model = new ActivityViewModel
+            {
+                Id = activity.Id,
+                Name = activity.Name,
+                Description = activity.Description
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new ActivityViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ActivityViewModel model)
+        {
+            var activity = Activity.Create(Guid.NewGuid(),
+                model.Name, model.Description);
+            _activityRepository.Add(activity);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            var actividad = _activityRepository.FindById(id);
+            var model = new ActivityViewModel
+            {
+                Id = actividad.Id,
+                Name = actividad.Name,
+                Description = actividad.Description
+            };
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            _activityRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
