97f31f90867e9d58fb81ec6ea8d4a222f864e0f1
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:40:29 +0200

    Lesson14

---
+++ FriendsTown.Web/Models/ActivityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FriendsTown.Web.Models;
+
+public class ActivityViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
