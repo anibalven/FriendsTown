d06b53a0014fad8ace3ba402f01b0468ba0fe485
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:13:50 +0200

    Lesson8

---
+++ FriendsTown.Web/Controllers/MyPocoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsTown.Web.Controllers;
+
+public class MyPocoController 
+{
+    public IActionResult Index()
+    {
+        return new ContentResult { 
+                Content = "This is a Poco Controller!" 
+            };
+    }
+}
+
