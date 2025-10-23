a17e71335d099ad971497f52c575ff3d205ef3ea
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:35:30 +0200

    Lesson12

---
+++ FriendsTown.Domain/Offer.cs
@@ -0,0 +1,41 @@
+﻿namespace FriendsTown.Domain;
+
+public class Offer : Entity<Guid>
+{
+    private Offer(Guid id) : base(id) { }
+
+    public Event Event { get; private set; }
+    public string Description { get; private set; }
+
+    private static void Validate(Event activityEvent, 
+        string description)
+    {
+        if (activityEvent is null)
+            throw new ArgumentException("Event is required");
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Name is required");
+    }
+
+    public void Update(Event activityEvent, string description)
+    {
+        Validate(activityEvent, description);
+
+        Description = description;
+        Event = activityEvent;
+    }
+
+    public static Offer Create(Guid id, Event theEvent,
+        string description)
+    {
+        Validate(theEvent, description);
+        Offer offer = new Offer(id)
+        {
+            Event = theEvent,
+            Description = description   
+        };
+
+        return offer;
+    }
+}
+
+
