6f5a43b48a5685bf579f664b3f0d12a769097357
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:26:53 +0200

    Lesson10

---
+++ FriendsTown.Web/Controllers/ActivityController.cs
@@ -0,0 +1,22 @@
+﻿using FriendsTown.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsTown.Web.Controllers;
+
+public class ActivityController : Controller
+{
+    public IActionResult Index()
+    {
+        var model = new List<ActivityViewModel>
+        {
+            new ActivityViewModel {Id=Guid.NewGuid(), Name="Climbing",
+            Description="Climb local peaks"},
+            new ActivityViewModel {Id=Guid.NewGuid(), Name="Hiking",
+            Description="walk through natural trails"},
+            new ActivityViewModel {Id=Guid.NewGuid(), Name="Yoga",
+            Description="Activity in a park"},
+        };
+        return View(model);
+    }
+}
+
