f572871aba330383003e01c8a8c29e903b28a6bf
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:50:16 +0200

    Lesson15

---
+++ FriendsTown.Domain/Event.cs
@@ -0,0 +1,65 @@
+﻿namespace FriendsTown.Domain;
+
+public class Event : Entity<Guid>
+{
+    public Event(Guid id) : base(id) { }
+
+    public Friend Organizer { get; protected set; }
+    public Activity ActivityType { get; protected set; }
+    public Date Date { get; protected set; }
+    public Place Place { get; protected set; }
+    public List<Friend> Participants { get; protected set; } = new();
+    public List<Offer> Offers { get; protected set; } = new ();
+
+    private void Validate(Friend organizer, Activity activity,
+        Date date, Place place)
+    {
+        if (organizer is null)
+            throw new ArgumentException("Organizer is required");
+        if (activity is null)
+            throw new ArgumentException("Activity is required");
+        if (date is null)
+            throw new ArgumentException("Date is required");
+        if (place is null)
+            throw new ArgumentException("Place is required");
+    }
+    public void Update(Friend organizer, Activity activity,
+        Date date, Place place)
+    {
+        Validate(organizer, activity, date, place);
+
+        Organizer = organizer;
+        ActivityType = activity;
+        Date = date;
+        Place = place;
+    }
+
+    public static Event Create(Guid id, Friend organizer,
+      Activity activity, Date date, Place place, 
+      List<string> offers)
+    {
+        Event activityEvent = new Event(id)
+        {
+            Organizer = organizer,
+            ActivityType = activity,
+            Date = date,
+            Place = place,
+        };
+
+        return activityEvent;
+    }
+
+    public void AddParticipant(Friend participant)
+    {
+        Participants.Add(participant);
+    }
+
+    public void RemoveParticipant(Friend participant)
+    {
+        if (Participants.Contains(participant))
+        {
+            Participants.Remove(participant);
+        }
+    }
+}
+
---
+++ FriendsTown.Web/Program.cs
@@ -0,0 +1,43 @@
+using FriendsTown.Data;
+using FriendsTown.Data.Repositories;
+using FriendsTown.Transversal;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
+
+
+builder.Services.AddDbContext<FriendsTownContext>(options => options
+    .UseSqlServer(builder.Configuration
+    .GetConnectionString("FriendsTown")), ServiceLifetime.Singleton);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+app.UseAuthorization();
+
+using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbContext = scope.ServiceProvider
+                         .GetRequiredService<FriendsTownContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
+
