5e455b5fc17efb361188a6dfca1340b70d597c2c
Anibal Marcano <anibalven@hotmail.com>
2025-10-23 20:19:31 +0200

    Lesson9

---
+++ FriendsTown.Web/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendsTown.Web.Models
+{
+    public class ProductViewModel
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+ 
